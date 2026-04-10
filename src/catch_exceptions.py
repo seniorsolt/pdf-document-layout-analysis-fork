cdf7b50bfcb4dca20c74cee0ef0f10b5bc2d6261
seniorsolt <102902511+seniorsolt@users.noreply.github.com>
2026-04-10 22:05:51 +0300

    fix logging for exceptions

--- src/catch_exceptions.py
+++ src/catch_exceptions.py
@@ -7,17 +7,20 @@ from configuration import service_logger
 def catch_exceptions(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
+        filename = kwargs["file"].filename if kwargs.get("file") else None
         try:
             service_logger.info(f"Calling endpoint: {func.__name__}")
-            if kwargs and "file" in kwargs:
-                service_logger.info(f"Processing file: {kwargs['file'].filename}")
-            if kwargs and "xml_file_name" in kwargs:
+            if filename:
+                service_logger.info(f"Processing file: {filename}")
+            if kwargs.get("xml_file_name"):
                 service_logger.info(f"Asking for file: {kwargs['xml_file_name']}")
             return await func(*args, **kwargs)
         except FileNotFoundError:
             raise HTTPException(status_code=404, detail="No xml file")
         except Exception:
-            service_logger.error("Error see traceback", exc_info=1)
+            service_logger.error(
+                f"Error processing file: {filename or '<unknown>'}", exc_info=1
+            )
             raise HTTPException(status_code=422, detail="Error see traceback")
 
     return wrapper
